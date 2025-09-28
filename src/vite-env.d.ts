4ab244221a95d1e3b229ac30d39cccfa267a977b
fixpt <57822954+fixpt@users.noreply.github.com>
2025-09-27 21:05:56 -0400

    Fix data base URL env stuff

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,2 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
-
-interface ViteTypeOptions {
-  strictImportMetaEnv: unknown;
-}
-
-interface ImportMetaEnv {
-  readonly VITE_DATAFILE_BASE_URL?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
@@ -6,10 +8,10 @@ export default defineConfig({
   plugins: [svelte()],
   experimental: {
     renderBuiltUrl(filename) {
-      if (import.meta.env.VITE_DATAFILE_BASE_URL != null) {
+      if (process.env.DATA_BASE_URL != null) {
         const match = filename.match(/^assets\/(.*\.bin)$/);
         if (match) {
-          return import.meta.env.VITE_DATAFILE_BASE_URL + match[1];
+          return process.env.DATA_BASE_URL + match[1];
         }
       }
     },
